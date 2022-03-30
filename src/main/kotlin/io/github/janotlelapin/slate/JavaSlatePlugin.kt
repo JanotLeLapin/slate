63a18c6e66f6c833a3e44528fd704cb5b49ba598
JanotLeLapin <51287094+JanotLeLapin@users.noreply.github.com>
2022-03-30 15:08:29 +0200

    Prevent unloading game world chunks

--- src/main/kotlin/io/github/janotlelapin/slate/JavaSlatePlugin.kt
+++ src/main/kotlin/io/github/janotlelapin/slate/JavaSlatePlugin.kt
@@ -7,12 +7,15 @@ import io.github.janotlelapin.slate.util.isGameDead
 import org.bukkit.event.EventHandler
 import org.bukkit.event.Listener
 import org.bukkit.event.entity.PlayerDeathEvent
+import org.bukkit.event.world.ChunkUnloadEvent
 import org.bukkit.plugin.java.JavaPlugin
 
 class JavaSlatePlugin : Listener, SlatePlugin, JavaPlugin() {
     override val gameManager: SlateGameManager = SlateGameManager()
 
     override fun onEnable() {
+        server.pluginManager.registerEvents(this, this)
+
         logger.info("Enabling Slate")
     }
 
@@ -24,5 +27,11 @@ class JavaSlatePlugin : Listener, SlatePlugin, JavaPlugin() {
     fun onPlayerDeath(e: PlayerDeathEvent) {
         val game = e.entity.game<GameSettings>() ?: return
         e.entity.isGameDead(true, game.plugin)
+    }
+
+    @EventHandler
+    fun onChunkUnload(e: ChunkUnloadEvent) {
+        if (e.world.game<GameSettings>() == null) return
+        e.isCancelled = true
     }
 }
