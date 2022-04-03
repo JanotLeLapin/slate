4c572fed61efcf3751e8b77dd5c13ec722151f11
JanotLeLapin <51287094+JanotLeLapin@users.noreply.github.com>
2022-04-03 12:48:22 +0200

    Create preset scenarios

---
+++ slate-api/src/main/kotlin/io/github/janotlelapin/slate/scenarios/CutCleanScenario.kt
@@ -0,0 +1,41 @@
+package io.github.janotlelapin.slate.scenarios
+
+import io.github.janotlelapin.slate.event.GameListener
+import io.github.janotlelapin.slate.game.Game
+import io.github.janotlelapin.slate.util.setDrops
+import org.bukkit.Material
+import org.bukkit.event.EventHandler
+import org.bukkit.event.block.BlockBreakEvent
+import org.bukkit.event.entity.EntityDeathEvent
+import org.bukkit.inventory.ItemStack
+
+/**
+ * A scenario that replaces raw meat/ores into their cooked form when obtained
+ */
+class CutCleanScenario : GameListener {
+    @EventHandler
+    fun onBlockBreak(e: BlockBreakEvent, game: Game<*>) {
+        val material = when (e.block.type) {
+            Material.IRON_ORE -> Material.IRON_INGOT
+            Material.GOLD_ORE -> Material.GOLD_INGOT
+            else -> null
+        } ?: return
+
+        e.setDrops(arrayListOf(ItemStack(material)))
+    }
+
+    @EventHandler
+    fun onEntityDeath(e: EntityDeathEvent, game: Game<*>) {
+        for (i in e.drops.indices) {
+            val material = when (e.drops[i].type) {
+                Material.RAW_BEEF -> Material.COOKED_BEEF
+                Material.RAW_CHICKEN -> Material.COOKED_CHICKEN
+                Material.RABBIT -> Material.COOKED_RABBIT
+                Material.MUTTON -> Material.COOKED_MUTTON
+                else -> null
+            } ?: continue
+
+            e.drops[i] = ItemStack(material)
+        }
+    }
+}
---
+++ slate-api/src/main/kotlin/io/github/janotlelapin/slate/scenarios/HastyBoysScenario.kt
@@ -0,0 +1,27 @@
+package io.github.janotlelapin.slate.scenarios
+
+import io.github.janotlelapin.slate.event.GameListener
+import io.github.janotlelapin.slate.game.Game
+import org.bukkit.enchantments.Enchantment
+import org.bukkit.event.EventHandler
+import org.bukkit.event.inventory.PrepareItemCraftEvent
+import org.bukkit.inventory.ItemStack
+
+class HastyBoysScenario : GameListener {
+    private val items = arrayOf(
+        "AXE",
+        "PICKAXE",
+        "SPADE",
+    )
+
+    @EventHandler
+    fun onCraft(e: PrepareItemCraftEvent, game: Game<*>) {
+        val result = e.recipe.result.type
+        val tokens = result.name.split("_")
+        if (tokens.size < 2 || !items.contains(tokens[1])) return
+
+        val it = ItemStack(result)
+        it.addEnchantment(Enchantment.DIG_SPEED, 3)
+        e.inventory.setItem(0, it)
+    }
+}
