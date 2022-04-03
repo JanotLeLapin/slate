01ea7598f58aedb305e05f3807aeb165b7a3fb65
JanotLeLapin <51287094+JanotLeLapin@users.noreply.github.com>
2022-04-03 18:01:52 +0200

    Hopeless attempt to fix random coordinates

--- slate-api/src/main/kotlin/io/github/janotlelapin/slate/util/Util.kt
+++ slate-api/src/main/kotlin/io/github/janotlelapin/slate/util/Util.kt
@@ -217,17 +217,13 @@ fun World.ground(x: Int, z: Int, start: Int = 48, end: Int = 255): Int? {
 fun World.randomCoordinates(range: Int = 500): Location {
     fun random() = ThreadLocalRandom.current().nextInt(-range, range)
 
-    cords@
     while (true) {
         val x = random()
         val z = random()
         val y = ground(x, z) ?: continue
 
-        for (i in 0..4) {
-            if (getBlockAt(x, y + i, z).type != Material.AIR) continue@cords
-        }
-
-        return Location(this, x + .5, y.toDouble(), z + .5)
+        if ((0..4).map { getBlockAt(x, y + it, z).type }.all { it == Material.AIR })
+            return Location(this, x + .5, y.toDouble(), z + .5)
     }
 }
 
